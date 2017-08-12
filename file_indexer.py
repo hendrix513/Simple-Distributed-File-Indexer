7371303f4b54f1e27e309d94341a5f5be9257333
Michael Schwartz <schwartzmichaels@yahoo.com>
2017-08-12 16:18:42 -0500

    Update file_indexer.py

--- file_indexer.py
+++ file_indexer.py
@@ -1,9 +1,18 @@
 import re
+from collections import defaultdict, Counter
+
 pattern = re.compile('[^a-zA-Z0-9]+')
 
-def blobToWords(blobStr):
+def blobToWords(blobStr: str)-> list:
     words = pattern.split(blobStr)
     for i in (0, -1):
-        if not l[i]:
-            l.pop(i)
-    return l
+        if not words[i]:
+            words.pop(i)
+    return words
+
+def topTenWordCounts(words: list)-> list:
+    counts = defaultdict(int)
+    for word in words:
+        counts[word.lower()] += 1
+        
+    return Counter(counts).most_common(10)

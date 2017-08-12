329b99d78ad56211f23b665adee2f15d3cb39a69
Michael Schwartz <schwartzmichaels@yahoo.com>
2017-08-12 16:52:45 -0500

    Update file_indexer.py

--- file_indexer.py
+++ file_indexer.py
@@ -3,17 +3,23 @@ from collections import defaultdict, Counter
 
 pattern = re.compile('[^a-zA-Z0-9]+')
 
-def blobToWords(blobStr: str)-> list:
+def blobToWords(blobStr)
     words = pattern.split(blobStr)
     for i in (0, -1):
         if not words[i]:
             words.pop(i)
     return words
 
-def topTenWordCounts(words: list)-> list:
+def topTenWordCounts(words)
     c = defaultdict(int)
     for word in words:
         c[word.lower()] += 1
         
     return sorted(c.items(), key=lambda x: x[1],
                   reverse=True)[:10]
+
+def main():
+    print 'Top Ten Word Counts: {0}'.format(wordCounter(docs))
+    
+if __name__ == '__main__':
+    main()

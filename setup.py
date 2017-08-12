6b935f78238f078996284ea75954233a3615e5ff
Michael Schwartz <schwartzmichaels@yahoo.com>
2017-08-12 17:51:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+    name='simple-distributed-file-indexer',
+    version='0.1',
+    author='Michael Schwartz',
+    scripts=['script/sdfi']
+)
